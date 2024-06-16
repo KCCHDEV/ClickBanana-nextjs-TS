7376acff455b14aaeb5688e095b28baf7b2130e4
Alone and Lonely dev <golfdx34@gmail.com>
2024-06-16 20:46:17 +0700

    add

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from 'react';
+
+export default function Home() {
+  const [A, setA] = useState(0);
+
+  const clickBanana = () => {
+    setA(A + 1);
+    const audio = new Audio('/1.mp3');
+    audio.volume = 0.2;
+    audio.play();
+  };
+
+  return (
+    <main className="flex justify-center flex-col items-center min-h-screen bg-yellow-200">
+      <h1 className="text-[3vw]">จำนวนการกด {A} ครั้ง</h1>
+      <button onClick={clickBanana} className="text-[20vw] transition ease-in-out duration-200 hover:scale-105 active:scale-75">🍌</button>
+      <a href='https://github.com/KCCHDEV' className='absolute top-0'>Make By NayGolf@MakiShop Team : V0.3bata</a>
+    </main>
+  );
+}
